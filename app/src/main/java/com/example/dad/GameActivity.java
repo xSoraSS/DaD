8c77b95754c10c714735438d27889dbba5c611ab
dam2a <amateu@elpuig.xeill.net>
2019-10-24 19:11:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dad/GameActivity.java
@@ -0,0 +1,14 @@
+package com.example.dad;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+}
