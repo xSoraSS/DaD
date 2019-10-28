7a6716a3ec618a13f6b2b3de144beb6efe17cb6c
dam2a <amateu@elpuig.xeill.net>
2019-10-28 20:11:43 +0100

    DaD

--- app/src/main/java/com/example/dad/GameActivity.java
+++ app/src/main/java/com/example/dad/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.dad;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.game_activity);
 
         Button opcionA = findViewById(R.id.opcionA); //Atlas Acompañará
--- app/src/main/java/com/example/dad/MainActivity.java
+++ app/src/main/java/com/example/dad/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         Button startGame = findViewById(R.id.startGame);
